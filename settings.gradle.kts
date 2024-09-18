2278283c8bfd10767595bea3bab078953c6ca110
Mahlatsi Ramano <ST10209387@rcconnect.edu.za>
2024-09-18 13:27:20 +0000

    new

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OPEN SOURCE CODING P2"
+include(":app")
+ 
